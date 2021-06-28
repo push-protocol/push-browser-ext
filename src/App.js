4ebcdb3c2e9d3fe9b73079dd77dd5f4a73bc53cf
akp111 <pradhanashish95@gmail.com>
2021-06-28 11:55:54 +0530

    removed consoles and commented codes

--- src/App.js
+++ src/App.js
@@ -21,13 +21,7 @@ function App() {
   const [walletAddr, setWalletAddr] =useState(null)
   useEffect(() => {
     const { component, props } = getCurrent()
-    console.log(
-      component
-        ? `There is a component on the stack! ${component} with ${props}`
-        : `The current stack is empty so Router's direct children will be rendered`,
-    )
     const components = getComponentStack()
-    console.log(`The stack has ${components.length} components on the stack`)
     chrome.storage.local.get(['epns'], function (result) {
       console.log(result.epns)
       if (result.epns) {
